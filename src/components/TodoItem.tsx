67481843fead5a5ccc38989d07afcf6a1521035e
GeunYeong <jgy091411@gmail.com>
2025-07-07 20:22:24 +0900

    ✨ TodoItem 컴포넌트 구현

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,19 @@
+import { useTodos } from "../hooks/useTodos";
+
+function TodoItem({ todo }) {
+  const { removeTodo, toggleTodo } = useTodos();
+
+  return (
+    <li key={todo.id}>
+      <input
+        type="checkbox"
+        onChange={() => toggleTodo(todo.id)}
+        checked={todo.checked}
+      />
+      {JSON.stringify(todo.checked)} / {todo.id} / {todo.text}
+      <button onClick={() => removeTodo(todo.id)}>X</button>
+    </li>
+  );
+}
+
+export default TodoItem;
